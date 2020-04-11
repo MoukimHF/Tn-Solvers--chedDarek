7830848069815977b2d82ba0b684970455e7ec88
MoukimHF <moukimhfaid@gmail.com>
2020-04-11 07:07:32 +0200

    many things
    many things

--- Rahma_sans_internet/index.php
+++ Rahma_sans_internet/index.php
@@ -1,41 +1,8 @@
 <!DOCTYPE html>
 <html lang="fr">
-
-<head>
-
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta property="og:title" content="tnsolvers" />
-    <meta property="og:description" content="Transformez les quartiers de tunisie en quartiers intelligentes" />
-
-    <title>TnSolvers</title>
-    <meta name="description" content="Devenir réalisable: aider et répondre aux demandes de renseignements enregistrées par téléphone pour les voisins plus âgés.">
-    <!-- Bootstrap core CSS -->
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom fonts for this template -->
-    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="vendor/simple-line-icons/css/simple-line-icons.css">
-    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Muli" rel="stylesheet">
-
-    <!-- Plugin CSS -->
-    <link rel="stylesheet" href="device-mockups/device-mockups.min.css">
-
-    <!-- Custom styles for this template -->
-    <link href="css/new-age.min.css" rel="stylesheet">
-
-    <!-- Favicon -->
-    <link rel="icon" href="/img/favicon/edited.png" type="image/png" sizes="16x16">
-
-    <link rel="manifest" href="/manifest.json">
-    <meta name="msapplication-TileColor" content="#ffffff">
-    <meta name="msapplication-TileImage" content="/img/favicon/ms-icon-144x144.png">
-    <meta name="theme-color" content="#ffffff">
-
-</head>
-
+<?php
+include('head.php');
+?>
 <body id="page-top">
     <!-- Mailjet -->
     <script type="text/javascript" src="https://app.mailjet.com/statics/js/iframeResizer.min.js"></script>
@@ -63,42 +30,9 @@
     </script>
     <noscript><p><img src="" style="border:0;" alt="" /></p></noscript>
     <!-- End Matomo Code -->
-
-
-    <!-- Navigation -->
-    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
-        <div class="container">
-            <a class="navbar-brand js-scroll-trigger" href="index.html#page-top">
-                <img id="logo" src="img/edited.png" width="80px" height="auto" alt="logo">
-            </a>
-            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-            Menu
-            <i class="fas fa-bars"></i>
-        </button>
-            <div class="collapse navbar-collapse" id="navbarResponsive">
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item">
-                        <a class="nav-link js-scroll-trigger font-weight-bold" href="#project">Le projet</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link js-scroll-trigger font-weight-bold" href="#howitworks">VOICI COMMENT ÇA MARCHE</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link js-scroll-trigger font-weight-bold" href="#about-us">À PROPOS DE NOUS</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link js-scroll-trigger font-weight-bold" href="faq.html">FAQ</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link js-scroll-trigger font-weight-bold" href="#contact">CONTACTEZ-NOUS</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link js-scroll-trigger font-weight-bold" href="#features">TÉLÉCHARGEZ L'APPLICATION</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+    <?php
+include('nav.php');
+?>
 
     <section class="project" id="project">
         <div class="container">
@@ -342,26 +276,27 @@
                     <img src="img/team/moukim.jpg" class="img-team" style="height: 120px; width: 120px; border-radius: 50%">
                     <div class="text-center">
                         <h4>Moukim</h4>
-                        <span class="d-block font-weight-bold">Etudiant</span>
-                        <span class="d-block">Tech lover
+                        <span class="d-block font-weight-bold">Etudiant FSEGN</span>
+                        <span class="d-block">JS lover
                         
                       </span>
                         <span class="d-block">
                         <a class="about-social" href="https://www.linkedin.com/in/moukim"><i
                                 class="fab fa-linkedin-in"></i></a>
+                                <a class="about-social" href="https://github.com/MoukimHF/"><i class="fab fa-github"></i></a>
                     </span>
                     </div>
                 </div>
                 <div class="col-12 col-sm-4 col-lg-3 mb-3 mt-3">
                     <img src="img/team/hayder.jpg" style="height: 120px; width: 120px; border-radius: 50%">
                     <div class="text-center">
-                        <h4>Moukim hafthi</h4>
+                        <h4>Hayder hedfi</h4>
                         <span class="d-block font-weight-bold">Etudiant</span>
                         <span class="d-block"></span>
                         <span class="d-block">
                         <a class="about-social" href="https://www.linkedin.com/in/benedikt-bleuel-81a18b1a5/"><i
                                 class="fab fa-linkedin"></i></a>
-                        <a class="about-social" href="https://github.com/moukim/"><i class="fab fa-github"></i></a>
+                        <a class="about-social" href="https://github.com/hayderhedfi/"><i class="fab fa-github"></i></a>
                       </span>
                     </div>
                 </div>
@@ -371,54 +306,10 @@
     </section>
 
 
+<?php
 
-    <footer>
-        <div class="container">
-            <p>&copy; 2020. All Rights Reserved.</p>
-            <p>Created with ❤</p>
-            <span class="d-block">
-                <a class="mr-3" href="impressum.html">Mentions</a>
-                <a class="mr-3" href="#">légales</a>
-                <a href="faq.html">FAQ sur la confidentialité</a>
-            </span>
-        </div>
-    </footer>
-
-    <!-- Bootstrap core JavaScript -->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-    <!-- Plugin JavaScript -->
-    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-
-    <!-- Custom scripts for this template -->
-    <script src="js/new-age.min.js"></script>
-    <script>
-        let onScroll = function(e) {
-            if (window.innerWidth >= 992) {
-                let contains = document.getElementById('mainNav').classList.contains('navbar-shrink');
-                let image = contains ? 'img/edited.png' : 'img/edited.png';
-                document.getElementById('logo').setAttribute('src', image);
-            } else {
-                document.getElementById('logo').setAttribute('src', 'img/edited.png');
-            }
-        };
-        window.addEventListener('scroll', onScroll, {
-            passive: true
-        });
-
-        function toggleAppVideo(app) {
-            if (app === 'android') {
-                document.getElementById('app-video').setAttribute('src', 'Rahma.png');
-                document.getElementById('app-ios').classList.toggle('toggle-app-active');
-                document.getElementById('app-android').classList.toggle('toggle-app-active');
-            } else {
-                document.getElementById('app-video').setAttribute('src', 'Rahma.png');
-                document.getElementById('app-ios').classList.toggle('toggle-app-active');
-                document.getElementById('app-android').classList.toggle('toggle-app-active');
-            }
-        }
-    </script>
+include('footer.php');
+?>
 </body>
 
 </html>
---
+++ Rahma_sans_internet/nav.php
@@ -0,0 +1,34 @@
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+        <div class="container">
+            <a class="navbar-brand js-scroll-trigger" href="index.html#page-top">
+                <img id="logo" src="img/edited.png" width="80px" height="auto" alt="logo">
+            </a>
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            Menu
+            <i class="fas fa-bars"></i>
+        </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger font-weight-bold" href="#project">Le projet</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger font-weight-bold" href="#howitworks">VOICI COMMENT ÇA MARCHE</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger font-weight-bold" href="#about-us">À PROPOS DE NOUS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger font-weight-bold" href="faq.html">FAQ</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger font-weight-bold" href="#contact">CONTACTEZ-NOUS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger font-weight-bold" href="#features">TÉLÉCHARGEZ L'APPLICATION</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
